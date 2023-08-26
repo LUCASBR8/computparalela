ab4bceb5f036200b58cd530a03618b07cc8fe628
LUCASBR8 <94135889+LUCASBR8@users.noreply.github.com>
2023-08-26 11:41:29 -0300

    Create ex4.c

---
+++ lab02/ex4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char nome1[20], nome2[20], temp[20];
+    int comparacao;
+
+    printf(" Digite um nome\n");
+    scanf("%s", nome1);
+    printf("\n Digite outro nome:\n");
+    scanf("%s", nome2);
+
+    comparacao = strcmp(nome1, nome2);
+
+    if (comparacao > 0)
+    {
+        // Se nome1 vem depois de nome2, troca os nomes
+        strcpy(temp, nome1);
+        strcpy(nome1, nome2);
+        strcpy(nome2, temp);
+    }
+
+    printf("\nEm ordem alfabética ficariam: %s e %s\n", nome1, nome2);
+
+    return 0;
+}
