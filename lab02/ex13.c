161da6e7583909812f09276c1256f24a4fbdcafd
LUCASBR8 <94135889+LUCASBR8@users.noreply.github.com>
2023-08-30 15:00:19 -0300

    Update ex13.c

--- lab02/ex13.c
+++ lab02/ex13.c
@@ -1,3 +1,10 @@
+/*
+    Nome dos participantes do grupo:  
+Andre Franco Raineri 321.636-81
+Luana Miho Yasuda 421.486-77
+Lucas Kenzo Kawamoto 421.456-51
+*/
+
 #include <stdio.h>
 
 int remover(char st[],int target,int n){
