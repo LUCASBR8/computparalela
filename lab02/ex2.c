bb8e9f22e21775a84e0fa07bd45928b834734122
LUCASBR8 <94135889+LUCASBR8@users.noreply.github.com>
2023-08-30 15:00:47 -0300

    Update ex2.c

--- lab02/ex2.c
+++ lab02/ex2.c
@@ -1,3 +1,10 @@
+/*
+    Nome dos participantes do grupo:  
+Andre Franco Raineri 321.636-81
+Luana Miho Yasuda 421.486-77
+Lucas Kenzo Kawamoto 421.456-51
+*/
+
 #include <stdio.h>
 
 char media(float n1,float n2,float n3,float me){
