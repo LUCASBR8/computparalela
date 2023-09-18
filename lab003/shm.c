bcd884c1f7cb7df2f1a497c8ce08c4d21a982711
R2andre <94245280+R2andre@users.noreply.github.com>
2023-09-18 15:11:41 -0300

    Update shm.c

--- lab003/shm.c
+++ lab003/shm.c
@@ -7,25 +7,26 @@
 
 
 int main(void) {
-  int *a;
+  int *shared_memory1;
+  int *shared_memory2;
   int shmid;
   pid_t pid;
   shmid=shmget((key_t)1122,sizeof(int),0666|IPC_CREAT);
 
-  a=(int *)shmat(shmid,NULL,0);
-  *a=4;
-  printf("PARENT: value = %d\n",*a);
+  shared_memory1=(int *)shmat(shmid,NULL,0);
+  *shared_memory1=4;
+  printf("PARENT: value = %d\n",*shared_memory1);
   pid = fork();
    if (pid == 0) {
     shmid=shmget((key_t)1122,sizeof(int),0666);
-    a=shmat(shmid,NULL,0);
-    *a=*a+1;
-    printf("CHILD: value=value+1 = %d\n",*a);
+    shared_memory2=shmat(shmid,NULL,0);
+    *shared_memory2=*shared_memory2+1;
+    printf("CHILD: value=value+1 = %d\n",*shared_memory2);
     return 0;
 	}
 	else if (pid > 0) {
 		wait(NULL);
-    printf("PARENT: value = %d\n",*a);
+    printf("PARENT: value = %d\n",*shared_memory1);
 		return 0;
   }
 }
