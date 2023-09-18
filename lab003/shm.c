a41124376fb3eac8d9edcb1340482b6ac93dc853
R2andre <94245280+R2andre@users.noreply.github.com>
2023-09-18 15:12:52 -0300

    Update shm.c

--- lab003/shm.c
+++ lab003/shm.c
@@ -7,26 +7,26 @@
 
 
 int main(void) {
-  int *shared_memory1;
-  int *shared_memory2;
+  int *shared_memory_variable1;
+  int *shared_memory_variable2;
   int shmid;
   pid_t pid;
   shmid=shmget((key_t)1122,sizeof(int),0666|IPC_CREAT);
 
-  shared_memory1=(int *)shmat(shmid,NULL,0);
-  *shared_memory1=4;
-  printf("PARENT: value = %d\n",*shared_memory1);
+  shared_memory_variable1=(int *)shmat(shmid,NULL,0);
+  *shared_memory_variable1=4;
+  printf("PARENT: value = %d\n",*shared_memory_variable1);
   pid = fork();
    if (pid == 0) {
     shmid=shmget((key_t)1122,sizeof(int),0666);
-    shared_memory2=shmat(shmid,NULL,0);
-    *shared_memory2=*shared_memory2+1;
-    printf("CHILD: value=value+1 = %d\n",*shared_memory2);
+    shared_memory_variable2=shmat(shmid,NULL,0);
+    *shared_memory_variable2=*shared_memory_variable2+1;
+    printf("CHILD: value=value+1 = %d\n",*shared_memory_variable2);
     return 0;
 	}
 	else if (pid > 0) {
 		wait(NULL);
-    printf("PARENT: value = %d\n",*shared_memory1);
+    printf("PARENT: value = %d\n",*shared_memory_variable1);
 		return 0;
   }
 }
