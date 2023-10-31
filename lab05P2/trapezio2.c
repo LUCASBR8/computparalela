91ac40ccaf5cc4a5a44ab81d9f82d86238efaedb
LUCASBR8 <94135889+LUCASBR8@users.noreply.github.com>
2023-10-31 16:06:28 -0300

    Update trapezio2.c

--- lab05P2/trapezio2.c
+++ lab05P2/trapezio2.c
@@ -7,10 +7,13 @@ double f(double x) {
     return exp(x);
 }
 
-double trapezoidal_rule_parallel(double a, double b, int n) {
+double trapezoidal_rule_parallel(double a, double b, int n, int num_threads) {
     double h = (b - a) / n;
-    double *local_sums = (double *)malloc(sizeof(double) * omp_get_max_threads());
-    
+    double sum = 0.0;
+    double *local_sums = (double *)malloc(num_threads * sizeof(double));
+
+    omp_set_num_threads(num_threads);
+
     #pragma omp parallel
     {
         int tid = omp_get_thread_num();
@@ -23,24 +26,45 @@ double trapezoidal_rule_parallel(double a, double b, int n) {
         }
     }
 
-    double sum = 0.0;
-    for (int i = 0; i < omp_get_max_threads(); i++) {
+    for (int i = 0; i < num_threads; i++) {
         sum += local_sums[i];
     }
 
     sum += (f(a) + f(b)) / 2.0;
+    sum *= h;
 
     free(local_sums);
-    return sum * h;
+    return sum;
 }
 
 int main() {
-    double a = 0.0;   // Limite inferior
-    double b = 1.0;   // Limite superior
-    int n = 1000000;  // Número de subdivisões
+    int num_threads;
+    double a, b;
+    int n;
+    double result;
+    double total_time = 0.0;
 
-    double result = trapezoidal_rule_parallel(a, b, n);
+    printf("Digite o número de threads a serem usadas: ");
+    scanf("%d", &num_threads);
+
+    printf("Digite o limite inferior (a): ");
+    scanf("%lf", &a);
+
+    printf("Digite o limite superior (b): ");
+    scanf("%lf", &b);
+
+    printf("Digite o número de subdivisões (n): ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < 10; i++) {
+        double start_time = omp_get_wtime();
+        result = trapezoidal_rule_parallel(a, b, n, num_threads);
+        double end_time = omp_get_wtime();
+        total_time += (end_time - start_time);
+    }
+
     printf("Resultado: %f\n", result);
+    printf("Tempo médio de execução: %f segundos\n", total_time / 10.0);
 
     return 0;
 }
