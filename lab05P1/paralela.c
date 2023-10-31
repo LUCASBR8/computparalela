4b94e86ff14bfa279d4c4e0373e733dac875f1e4
LUCASBR8 <94135889+LUCASBR8@users.noreply.github.com>
2023-10-31 15:55:51 -0300

    Update paralela.c

--- lab05P1/paralela.c
+++ lab05P1/paralela.c
@@ -2,32 +2,56 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <time.h>
 
 double f(double x) {
-return exp(x);
+    return exp(x);
 }
 
-double trapezoidal_rule_parallel(double a, double b, int n) {
-double h = (b - a) / n;
-double sum = 0.0;
+double trapezoidal_rule_parallel(double a, double b, int n, int num_threads) {
+    double h = (b - a) / n;
+    double sum = 0.0;
 
-#pragma omp parallel for reduction(+:sum)
-for (int i = 1; i < n; i++) {
-double x = a + i * h;
-sum += f(x);
-}
-sum +=(f(a)+f(b))/2.0;
-return sum * h;
+    #pragma omp parallel num_threads(num_threads) 
+    {
+        int tid = omp_get_thread_num();
+        double local_sum = 0.0;
+
+        #pragma omp for
+        for (int i = 1; i < n; i++) {
+            double x = a + i * h;
+            local_sum += f(x);
+        }
+
+        #pragma omp critical
+        sum += local_sum;
+    }
+
+    sum += (f(a) + f(b)) / 2.0;
+    return sum * h;
 }
 
 int main() {
-double a = 0.0; // Limit inferior
-double b = 1.0; // Limit superior
-int n = 1000000; // Número de subdivisões
+    double a = 0.0; // Limite inferior
+    double b = 1.0; // Limite superior
+    int n = 1000000; // Número de subdivisões
 
-double result = trapezoidal_rule_parallel(a, b, n);
-printf("Resultado: %f\n", result);
+    int num_threads;
+    printf("Digite o número de threads: ");
+    scanf("%d", &num_threads);
 
-return 0;
+    double result;
+    clock_t start_time, end_time;
+
+    start_time = clock();
+    result = trapezoidal_rule_parallel(a, b, n, num_threads);
+    end_time = clock();
+
+    double total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    printf("Resultado: %f\n", result);
+    printf("Tempo de execução: %f segundos\n", total_time);
+
+    return 0;
 }
 
